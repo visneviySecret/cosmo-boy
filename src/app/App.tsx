8db521b3a1afe9ee19ae02452a7e7f3ba7d39623
E. Belousov <visneviy.secret@yandex.ru>
2025-05-27 17:34:16 +0300

    Инициализация приложения

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import MainPage from "../scenes/main/ui/MainPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
