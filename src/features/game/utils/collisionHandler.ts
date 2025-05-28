aa1578224600a9def646afcb7382780cbd4cfa95
E. Belousov <visneviy.secret@yandex.ru>
2025-05-28 19:11:35 +0300

    Переименнование астероидов

--- src/features/game/utils/collisionHandler.ts
+++ src/features/game/utils/collisionHandler.ts
@@ -1,7 +1,7 @@
 import { Player } from "../entities/Player";
-import { Meteorite } from "../entities/Meteorite";
+import { Asteroid } from "../entities/Asteroid";
 
-export function handleCollision(player: Player, meteorite: Meteorite) {
+export function handleCollision(player: Player, asteroid: Asteroid) {
   // Сохраняем скорости перед столкновением
   const playerVelocityX = player.body?.velocity.x || 0;
   const playerVelocityY = player.body?.velocity.y || 0;
@@ -11,11 +11,11 @@ export function handleCollision(player: Player, meteorite: Meteorite) {
   const playerMomentumY = playerVelocityY * player.getMass();
 
   // Вычисляем новую скорость метеорита (v = p/m)
-  const newVelocityX = playerMomentumX / meteorite.getMass();
-  const newVelocityY = playerMomentumY / meteorite.getMass();
+  const newVelocityX = playerMomentumX / asteroid.getMass();
+  const newVelocityY = playerMomentumY / asteroid.getMass();
 
   // Устанавливаем одинаковую скорость для метеорита и игрока
-  meteorite.setVelocity(newVelocityX, newVelocityY);
+  asteroid.setVelocity(newVelocityX, newVelocityY);
   player.setVelocity(newVelocityX, newVelocityY);
 
   // Устанавливаем флаг нахождения на метеорите
