80907012a51cfa2c93e8f9f77474760412d055db
E. Belousov <visneviy.secret@yandex.ru>
2025-06-05 19:39:18 +0300

    Девтулз для стора

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,7 +1,13 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { createGameSlice } from "./slices/gameSlice";
-import type { GameState } from "../types/game.ts";
+import type { GameState } from "../types/game";
 
-export const useStore = create<GameState>()((...args) => ({
-  ...createGameSlice(...args),
-}));
+export const useStore = create<GameState>()(
+  devtools(
+    (...args) => ({
+      ...createGameSlice(...args),
+    }),
+    { name: "GameStore" }
+  )
+);
