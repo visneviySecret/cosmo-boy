930e2a53d141b0af40beb04c5c77797c066eacbb
E. Belousov <visneviy.secret@yandex.ru>
2025-06-05 20:20:48 +0300

    Поправил стрелочку в селекторе

--- src/entities/PlatformTypeSelector/PlatformTypeSelector.style.ts
+++ src/entities/PlatformTypeSelector/PlatformTypeSelector.style.ts
@@ -26,6 +26,10 @@ export const PlatformIcon = styled.svg`
 export const Selector = styled.select`
   font-size: 1.2rem;
   border-radius: 8px;
-  padding: 1em 0.5rem 1em 3em;
-  /* left padding увеличен для иконки */
+  padding: 1em 2.5em 1em 3em;
+  appearance: none;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+  background-repeat: no-repeat;
+  background-position: right 1em center;
+  background-size: 1em;
 `;
